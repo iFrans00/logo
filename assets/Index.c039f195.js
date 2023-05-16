e371e8845263f849862b643628df4085d766ad89
iFrans00 <fransa0889@gmail.com>
2023-05-16 11:44:57 +0700

    Updates

---
+++ assets/Index.c039f195.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s}from"./index.c01ad529.js";const t={},c={class:""};function n(r,a,_,p,d,f){return o(),s("div",c)}const l=e(t,[["render",n]]);export{l as default};
--- assets/VContainer.26fa033a.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */import{m as t,u as n}from"./intersectionObserver.526b86ef.js";import{j as r,b as s}from"./index.004e47e7.js";const f=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{f as V};
